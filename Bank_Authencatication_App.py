88e379376ef57fd7667adb84af89e750793b3cc2
ghizzghiz <ghizlanerehioui@gmail.com>
2023-12-12 13:16:36 -0700

    update wavelet and app

--- Bank_Authencatication_App.py
+++ Bank_Authencatication_App.py
@@ -1,12 +1,9 @@
-# streamlit run 0_Bank_Authencaticate_App.py
-#import requirements
+# streamlit run Bank_Authencaticate_App.py
 import streamlit as st
 import numpy as np
 import pandas as pd
 from wavelet_transform import wavelet_trs
 from PIL import Image
-#import Classification_Algorithms # our Logistic Regression model
-#from Classification_Algorithms import LogiReg
 from sklearn.preprocessing import StandardScaler
 
 #from sklearn.externals 
@@ -14,12 +11,14 @@ import joblib
 # Load the saved Logistic Regression model
 loaded_model = joblib.load('finalized_LogiReg.sav')
 
-st.title('Banknote Authentication')
-st.write("""
-         
-Upload a picture of a US Banknote and find out if it is authentic or fake. 
+# Load the saved Random Forest model
+loaded_model = joblib.load('finalized_rf.sav')
 
-""")
+# Load the saved SVM model
+loaded_model = joblib.load('finalized_svm.sav')
+
+st.title('Banknote Authentication')
+st.write("Upload a picture of a US Banknote and find out if it is authentic or fake.")
 
 uploaded_file = st.file_uploader("Upload an image", accept_multiple_files=False, type = 'jpeg')
 
@@ -35,19 +34,7 @@ if uploaded_file is not None:
     st.subheader('Image After Processing')
     result = wavelet_trs(uploaded_file)
     st.write(result)
-
-    #### SOMETHING WRONG WITH THIS STANDARDIZATION
-    #scaler = StandardScaler()
-    #scaler.fit(pd.DataFrame(result).transpose())
-    #result = scaler.fit_transform(pd.DataFrame(result).transpose())
-
-    #result = pd.DataFrame(result)#.transpose()
-
-    #st.write(result)
-
-    #st.write(result.type)
-    #st.write(result.dtypes) # all floats 
-
+    
     predictions = loaded_model.predict(result)
     st.write('The prediction is that: ')
     st.write(predictions)
--- wavelet_transform.py
+++ wavelet_transform.py
@@ -1,4 +1,4 @@
-# 1. Import the required libraries:
+# Import the required libraries
 from PIL import Image
 import numpy as np
 import pandas as pd
@@ -12,54 +12,21 @@ import streamlit as st
 from Classification_Algorithms import X_means, X_stds
 
 def wavelet_trs(image):
-    # 2. Load the image in grayscale, resize, and change resolution
+    # Load the image in grayscale, resize, and change resolution
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.resize(image, (400, 400))
-    normalized_image = (image - np.mean(X_means+image)) / np.mean(X_stds+image) # CHECK AGAIN
-    normalized_image = Image.fromarray(normalized_image)
     st.image(image)
-    #### keep in mind the size of the images and quality
-    # https://auth0.com/blog/image-processing-in-python-with-pillow/
-    #image = change_dpi_array(image, 660)
-    
-    # 3. Perform the wavelet transform on the image:
-    coeffs = pywt.wavedec2(normalized_image, 'haar')  # Choose your desired wavelet (e.g., 'haar')
-    #coeffs
 
-    # 4. Extract the approximation (low-frequency) and detail (high-frequency) coefficients:
-    #approx_coeffs, detail_coeffs = coeffs[0], coeffs[1:]
-
-    # 5. Calculate the desired statistical features for each coefficient level:
-    #variance_ = [np.var(level) for level in detail_coeffs]
-    #skewness_ = [skew(level).flatten() for level in detail_coeffs]
-    #kurtosis_ = [kurtosis(level).flatten() for level in detail_coeffs]
-    #entropy_ = [entropy(np.abs(level).flatten()) for level in detail_coeffs]
-
-    # 6. Optionally, you can also calculate these features for the approximation coefficients:
-    #approx_variance = np.var(approx_coeffs)
-    #approx_skewness = skew(approx_coeffs.flatten())
-    #approx_kurtosis = kurtosis(approx_coeffs.flatten())
-    #approx_entropy = entropy(np.abs(approx_coeffs.flatten()))
-
-    #result = [approx_variance, approx_skewness, approx_kurtosis, approx_entropy]
-    ##result = [variance_, skewness_, kurtosis_, entropy_] # WRONG
-
-
-    ##st.write("Image Statistics - Mean:", np.mean(image), "Std Dev:", np.std(image))
+    # Perform the wavelet transform on the image
     coeffs = pywt.dwt2(image, 'haar')
     cA, (cH, cV, cD) = coeffs
 
     # Extract features
     features = {
-        'Variance': np.var(cA),
-        'Skewness': skew(cA.flatten()),
-        'Kurtosis': kurtosis(cA.flatten()),
-        'Entropy': entropy(cA.flatten())
+        'variance': np.var(cA),
+        'skewness': skew(cA.flatten()),
+        'kurtosis': kurtosis(cA.flatten()),
+        'entropy': entropy(cA.flatten())
     }
     
     return features
-
-# 7. You can then use these calculated features for further analysis or visualization.
-
-# Make sure to adjust the wavelet type and other parameters according to your specific requirements.
-# Additionally, you may need to normalize or scale the features depending on your application.
